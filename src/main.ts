1746ff4546e4532c75b2a53e54613207517a5bc5
Rezorwar <warwarwar56@gmail.com>
2025-04-18 16:06:06 +0200

    init Nest backend №6

--- src/main.ts
+++ src/main.ts
@@ -1,17 +1,24 @@
-// src/main.ts
+// В файле main.ts сервера NestJS
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import * as cookieParser from 'cookie-parser';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-  
+
+  // Настройка CORS
   app.enableCors({
     origin: process.env.CLIENT_URL || 'http://localhost:3000',
     methods: 'GET,HEAD,PUT,PATCH,POST,DELETE',
+    allowedHeaders: [
+      'Content-Type',
+      'Authorization',
+      'Cache-Control', // Добавляем разрешённые заголовки
+      'Pragma'
+    ],
     credentials: true,
-    allowedHeaders: ['Content-Type', 'Authorization'],
-    exposedHeaders: ['set-cookie']
+    preflightContinue: false,
+    optionsSuccessStatus: 204
   });
 
   app.use(cookieParser());
