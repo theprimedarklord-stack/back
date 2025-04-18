16d7d0cde9fa74a6eb8535dc41e351d611204829
Rezorwar <warwarwar56@gmail.com>
2025-04-18 12:11:33 +0200

    init Nest backend №2

--- src/supabase/supabase.service.ts
+++ src/supabase/supabase.service.ts
@@ -8,10 +8,14 @@ export class SupabaseService {
   private supabase: SupabaseClient;
 
   constructor(private configService: ConfigService) {
-    this.supabase = createClient(
-      this.configService.get('SUPABASE_URL'),
-      this.configService.get('SUPABASE_ANON_KEY')
-    );
+    const url = this.configService.get<string>('SUPABASE_URL');
+    const anonKey = this.configService.get<string>('SUPABASE_ANON_KEY');
+
+    if (!url || !anonKey) {
+      throw new Error('SUPABASE_URL or SUPABASE_ANON_KEY is not defined in environment variables.');
+    }
+
+    this.supabase = createClient(url, anonKey);
   }
 
   getClient() {
