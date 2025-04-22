430872896152ae2becd6609f28a6f71f058dc361
Rezorwar <warwarwar56@gmail.com>
2025-04-22 13:34:54 +0200

    init Nest backend №25

--- src/main.ts
+++ src/main.ts
@@ -19,10 +19,10 @@ async function bootstrap() {
 
   // Включаем CORS, разрешая куки и заголовок Set-Cookie
   app.enableCors({
-    // origin: clientUrl,
+    origin: clientUrl,
     // origin: true,
     // origin: ['http://localhost:3000'],
-    origin: ['http://localhost:3000', 'https://smartmemory.vercel.app'],
+    // origin: ['http://localhost:3000', 'https://smartmemory.vercel.app'],
     credentials: true,
     allowedHeaders: ['Content-Type', 'Authorization', 'X-Requested-With', 'Accept', 'Origin', 'Pragma'],
     exposedHeaders: ['Set-Cookie'],
