79c9f0bf0182ca57f3066fafc0ed9042018b5de4
Rezorwar <warwarwar56@gmail.com>
2025-04-22 10:51:00 +0200

    init Nest backend №14

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,27 +1,28 @@
-import { MiddlewareConsumer, Module, RequestMethod } from '@nestjs/common';
-import { AppController } from './app.controller';
-import { AppService } from './app.service';
-import { ConfigModule } from '@nestjs/config';
-import { SupabaseModule } from './supabase/supabase.module';
-import { AuthModule } from './auth/auth.module';
-import { AuthMiddleware } from './common/middleware/auth.middleware';
+// import { MiddlewareConsumer, Module, RequestMethod } from '@nestjs/common';
+// import { AppController } from './app.controller';
+// import { AppService } from './app.service';
+// import { ConfigModule } from '@nestjs/config';
+// import { SupabaseModule } from './supabase/supabase.module';
+// import { AuthModule } from './auth/auth.module';
+// import { AuthMiddleware } from './common/middleware/auth.middleware';
 
-@Module({
-  imports: [
-    ConfigModule.forRoot({ isGlobal: true }),
-    SupabaseModule,
-    AuthModule,
-  ],
-  controllers: [AppController],
-  providers: [AppService],
-})
-export class AppModule {
-  configure(consumer: MiddlewareConsumer) {
-    consumer
-      .apply(AuthMiddleware)
-      .forRoutes(
-        { path: 'auth/profile', method: RequestMethod.GET },
-        { path: 'account', method: RequestMethod.ALL },
-      );
-  }
-}
+// @Module({
+//   imports: [
+//     ConfigModule.forRoot({ isGlobal: true }),
+//     SupabaseModule,
+//     AuthModule,
+//   ],
+//   controllers: [AppController],
+//   providers: [AppService],
+// })
+// export class AppModule {
+//   configure(consumer: MiddlewareConsumer) {
+//     consumer
+//       .apply(AuthMiddleware)
+//       .forRoutes(
+//         { path: 'auth/profile', method: RequestMethod.GET },
+//         { path: 'account', method: RequestMethod.ALL },
+//       );
+//   }
+// }
+export class AppModule {}
