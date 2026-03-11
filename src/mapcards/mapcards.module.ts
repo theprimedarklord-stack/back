9c9892711c6e241a18122d338406bdebe83adadb
warezorwar <warezorwar@gmail.com>
2026-03-11 13:33:07 +0200

    peredelal mapcard na nest #4

--- src/mapcards/mapcards.module.ts
+++ src/mapcards/mapcards.module.ts
@@ -1,8 +1,10 @@
 import { Module } from '@nestjs/common';
 import { MapCardsController } from './mapcards.controller';
 import { MapCardsService } from './mapcards.service';
+import { AuthModule } from '../auth/auth.module';
 
 @Module({
+  imports: [AuthModule],
   controllers: [MapCardsController],
   providers: [MapCardsService],
 })
